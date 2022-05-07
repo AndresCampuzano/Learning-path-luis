a9977ea2ac0fd29f52699d0561167b4da1323042
Andres Campuzano <acampuzano@hugeinc.com>
2022-05-07 10:28:46 -0500

    initial commit, sunny web page css grid example

---
+++ app.js
@@ -0,0 +1,27 @@
+// const element = document.querySelector('#container')
+//
+// const data = [
+//     {
+//         id: 1,
+//         title: "Product 1",
+//         description: "I'm a fancy description 1."
+//     },
+//     {
+//         id: 2,
+//         title: "Product 2",
+//         description: "Asdasdasdasdn 2."
+//     },
+// ]
+//
+// function callEl() {
+//     for (let i = 0; i <= data.length; i++) {
+//         element.innerHTML += `
+//             <div id="${data[i].id}">
+//                 <h3>${data[i].title}</h3>
+//                 <span>${data[i].description}</span>
+//             </div>
+//         `;
+//     }
+// }
+//
+// callEl()
